ac96e45a70f43b4041a12c249e062475b44080cd
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-09-16 10:12:13 +0700

    Add education and experience form

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -1,12 +1,33 @@
+import { useState } from 'react';
+import InputField from './InputField';
 import '../styles/form.css'
 
+function EducationForm() {
+  return (
+    <form action="">
+      <InputField label='School / University Name' type='text'></InputField>
+      <InputField label='Degree / Title of Study' type='email'></InputField>
+      <InputField label='Start Date' type='date'></InputField>
+      <InputField label='End Date' type='date'></InputField>
+      <button type="submit">Save</button>
+    </form>
+  )
+}
+
 function EducationSection() {
+  const [expanded, setExpanded] = useState(false)
+
+  const handleExpand = () => {
+    setExpanded(true)
+  }
+  
   return (
     <section>
       <div className="form-header">
         <h2>Education</h2>
-        <span className="material-symbols-outlined">add</span>
+        <span className="material-symbols-outlined" onClick={handleExpand}>add</span>
       </div>
+      {expanded ? <EducationForm></EducationForm> : ''}
     </section>
   )
 }
--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -1,12 +1,33 @@
+import { useState } from 'react';
+import InputField from './InputField';
 import '../styles/form.css'
 
+function ExperienceForm() {
+  return (
+    <form action="">
+      <InputField label='Place of Work' type='text'></InputField>
+      <InputField label='Title' type='email'></InputField>
+      <InputField label='Start Date' type='date'></InputField>
+      <InputField label='End Date' type='date'></InputField>
+      <button type="submit">Save</button>
+    </form>
+  )
+}
+
 function ExperienceSection() {
+  const [expanded, setExpanded] = useState(false)
+
+  const handleExpand = () => {
+    setExpanded(true)
+  }
+
   return (
     <section>
       <div className="form-header">
         <h2>Work Experience</h2>
-        <span className="material-symbols-outlined">add</span>
+        <span className="material-symbols-outlined" onClick={handleExpand}>add</span>
       </div>
+      {expanded ? <ExperienceForm></ExperienceForm> : ''}
     </section>
   )
 }
