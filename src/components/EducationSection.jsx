7cc66947a79519d711db242ba5acce1798de4dd6
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-09-16 11:09:56 +0700

    Add cancel button

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react';
 import InputField from './InputField';
 import '../styles/form.css'
 
-function EducationForm() {
+function EducationForm({handleCancel}) {
   return (
     <form action="">
       <InputField label='School / University Name' type='text'></InputField>
       <InputField label='Degree / Title of Study' type='email'></InputField>
       <InputField label='Start Date' type='date'></InputField>
       <InputField label='End Date' type='date'></InputField>
-      <button type="submit">Save</button>
+      <div className="btn-container">
+        <button className='cancel-btn' onClick={handleCancel}>Cancel</button>
+        <button className='submit-btn' type='submit'>Save</button>
+      </div>
     </form>
   )
 }
@@ -20,6 +23,10 @@ function EducationSection() {
   const handleExpand = () => {
     setExpanded(true)
   }
+
+  const handleCancel = () => {
+    setExpanded(false)
+  }
   
   return (
     <section>
@@ -27,7 +34,7 @@ function EducationSection() {
         <h2>Education</h2>
         <span className="material-symbols-outlined" onClick={handleExpand}>add</span>
       </div>
-      {expanded ? <EducationForm></EducationForm> : ''}
+      {expanded ? <EducationForm handleCancel={handleCancel}></EducationForm> : null}
     </section>
   )
 }
--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react';
 import InputField from './InputField';
 import '../styles/form.css'
 
-function ExperienceForm() {
+function ExperienceForm({handleCancel}) {
   return (
     <form action="">
       <InputField label='Place of Work' type='text'></InputField>
       <InputField label='Title' type='email'></InputField>
       <InputField label='Start Date' type='date'></InputField>
       <InputField label='End Date' type='date'></InputField>
-      <button type="submit">Save</button>
+      <div className="btn-container">
+        <button className='cancel-btn' onClick={handleCancel}>Cancel</button>
+        <button className='submit-btn' type='submit'>Save</button>
+      </div>
     </form>
   )
 }
@@ -21,13 +24,17 @@ function ExperienceSection() {
     setExpanded(true)
   }
 
+  const handleCancel = () => {
+    setExpanded(false)
+  }
+
   return (
     <section>
       <div className="form-header">
         <h2>Work Experience</h2>
         <span className="material-symbols-outlined" onClick={handleExpand}>add</span>
       </div>
-      {expanded ? <ExperienceForm></ExperienceForm> : ''}
+      {expanded ? <ExperienceForm handleCancel={handleCancel}></ExperienceForm> : null}
     </section>
   )
 }
