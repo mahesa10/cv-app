d842dae07a32780fe590d109f323678a1a0e2942
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-09-17 22:39:55 +0700

    Add cv header layout

--- src/components/CVResult.jsx
+++ src/components/CVResult.jsx
@@ -2,7 +2,15 @@ import '../styles/cv-result.css'
 
 function CVResult() {
   return (
-    <div className="cv-result"></div>
+    <div className="cv-result">
+      <div className="cv-header">
+        <h2 className="full-name">John Doe</h2>
+        <div className="person-info">
+          Jakarta, Indonesia | +6281234567890 | johndoe@gmail.com
+        </div>
+        <div className="personal-web">linkedin.com/johndoe</div>
+      </div>
+    </div>
   )
 }
 
